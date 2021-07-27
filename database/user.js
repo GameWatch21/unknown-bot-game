fabada425a6d4c5242277a48fc6d6a2bdce44c1c
Glitch (codeground) <none>
2021-07-27 15:28:34 +0000

    just some edit

--- database/user.js
+++ database/user.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const profileSchema = mongoose.Schema({
    Username: String,
-   ID: Number,
+   ID: { type: Number, require: true },
    RegisteredAt: String,
-   Money: Number,
-   Power: Number,
-   Attack: Number,
-   Defense: Number,
+   Money: { type: Number, default: 0},
+   Power: { type: Number, default: 10},
+   Attack: { type: Number, default: 5},
+   Defense: { type: Number, default: 0}
   });
 
 const profileModel = mongoose.model('profile', profileSchema);
