71be1d2ce4f6a80e1bbd9da749eabb6aab53ba81
Glitch (codeground) <none>
2021-07-28 00:09:47 +0000

    morning, i think

---
+++ freaking-commands-here/adventure.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+
+module.exports = {
+  name: "adventure",
+  description: "adventuring go brr",
+  execute(message, args){
+    
+    
+    const adventure = new Discord.MessageEmbed()
+    .setTitle("Adventure")
+    .addFields(
+      {
+        name: "Username:" , 
+        value: "nan"
+      },
+      {
+        name: "Energy" ,
+        value: "nan"
+      },
+      {
+        name: "Gold Earned",
+        value: "nan"
+      },
+      {
+        name: "Unknown Killed",
+        value: "nan"
+      }
+       )
+    .setFooter("Requested by" + message.author.username)
+    .setColor("GREEN");
+    
+    }
+  }
